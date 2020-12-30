1677b07708090b1e0cc49483f46c0935391e4775
Tanjim Ahmed <53538004+TanjimAnim@users.noreply.github.com>
2020-12-30 16:02:40 +0600

    Add files via upload

---
+++ data/dailyData.js
@@ -0,0 +1,282 @@
+export default [
+  {
+    dt: 1609131600,
+    sunrise: 1609115974,
+    sunset: 1609154405,
+    temp: {
+      day: 23.05,
+      min: 15.37,
+      max: 25.91,
+      night: 19.92,
+      eve: 23.16,
+      morn: 16.06
+    },
+    feels_like: {
+      day: 21.14,
+      night: 18.47,
+      eve: 20.48,
+      morn: 13.93
+    },
+    pressure: 1014,
+    humidity: 39,
+    dew_point: 8.67,
+    wind_speed: 2.18,
+    wind_deg: 313,
+    weather: [
+      {
+        id: 800,
+        main: "Clear",
+        description: "clear sky",
+        icon: "01d"
+      }
+    ],
+    clouds: 0,
+    pop: 0,
+    uvi: 5.49
+  },
+  {
+    dt: 1609221600,
+    sunrise: 1609202396,
+    sunset: 1609240841,
+    temp: {
+      day: 25.14,
+      min: 15.86,
+      max: 26.42,
+      night: 18.65,
+      eve: 21.65,
+      morn: 15.86
+    },
+    feels_like: {
+      day: 22.57,
+      night: 15.65,
+      eve: 18.43,
+      morn: 14.64
+    },
+    pressure: 1014,
+    humidity: 34,
+    dew_point: 8.35,
+    wind_speed: 3.06,
+    wind_deg: 350,
+    weather: [
+      {
+        id: 800,
+        main: "Clear",
+        description: "clear sky",
+        icon: "01d"
+      }
+    ],
+    clouds: 0,
+    pop: 0,
+    uvi: 5.73
+  },
+  {
+    dt: 1609308000,
+    sunrise: 1609288816,
+    sunset: 1609327277,
+    temp: {
+      day: 24.77,
+      min: 15.66,
+      max: 26.66,
+      night: 18.75,
+      eve: 21.95,
+      morn: 15.86
+    },
+    feels_like: {
+      day: 22.87,
+      night: 16.22,
+      eve: 19.15,
+      morn: 14.45
+    },
+    pressure: 1014,
+    humidity: 38,
+    dew_point: 9.7,
+    wind_speed: 2.58,
+    wind_deg: 342,
+    weather: [
+      {
+        id: 800,
+        main: "Clear",
+        description: "clear sky",
+        icon: "01d"
+      }
+    ],
+    clouds: 0,
+    pop: 0,
+    uvi: 5.58
+  },
+  {
+    dt: 1609394400,
+    sunrise: 1609375235,
+    sunset: 1609413713,
+    temp: {
+      day: 25.14,
+      min: 15.46,
+      max: 26.59,
+      night: 19.71,
+      eve: 21.69,
+      morn: 15.62
+    },
+    feels_like: {
+      day: 22.62,
+      night: 17.12,
+      eve: 18.82,
+      morn: 13.74
+    },
+    pressure: 1014,
+    humidity: 34,
+    dew_point: 8.23,
+    wind_speed: 2.99,
+    wind_deg: 324,
+    weather: [
+      {
+        id: 800,
+        main: "Clear",
+        description: "clear sky",
+        icon: "01d"
+      }
+    ],
+    clouds: 0,
+    pop: 0,
+    uvi: 5.66
+  },
+  {
+    dt: 1609480800,
+    sunrise: 1609461653,
+    sunset: 1609500151,
+    temp: {
+      day: 24.99,
+      min: 15.45,
+      max: 26.66,
+      night: 19.81,
+      eve: 21.8,
+      morn: 15.45
+    },
+    feels_like: {
+      day: 22.46,
+      night: 17.01,
+      eve: 18.96,
+      morn: 13.87
+    },
+    pressure: 1015,
+    humidity: 33,
+    dew_point: 7.99,
+    wind_speed: 2.81,
+    wind_deg: 316,
+    weather: [
+      {
+        id: 800,
+        main: "Clear",
+        description: "clear sky",
+        icon: "01d"
+      }
+    ],
+    clouds: 7,
+    pop: 0,
+    uvi: 5.57
+  },
+  {
+    dt: 1609567200,
+    sunrise: 1609548070,
+    sunset: 1609586589,
+    temp: {
+      day: 24.77,
+      min: 15.66,
+      max: 26.57,
+      night: 19.68,
+      eve: 21.52,
+      morn: 15.66
+    },
+    feels_like: {
+      day: 22.56,
+      night: 17.33,
+      eve: 18.45,
+      morn: 13.98
+    },
+    pressure: 1014,
+    humidity: 34,
+    dew_point: 8.28,
+    wind_speed: 2.44,
+    wind_deg: 312,
+    weather: [
+      {
+        id: 800,
+        main: "Clear",
+        description: "clear sky",
+        icon: "01d"
+      }
+    ],
+    clouds: 0,
+    pop: 0,
+    uvi: 6
+  },
+  {
+    dt: 1609653600,
+    sunrise: 1609634485,
+    sunset: 1609673028,
+    temp: {
+      day: 25.02,
+      min: 15.78,
+      max: 26.77,
+      night: 19.97,
+      eve: 22.28,
+      morn: 15.78
+    },
+    feels_like: {
+      day: 22.64,
+      night: 17.5,
+      eve: 19.91,
+      morn: 14.42
+    },
+    pressure: 1013,
+    humidity: 33,
+    dew_point: 7.79,
+    wind_speed: 2.61,
+    wind_deg: 337,
+    weather: [
+      {
+        id: 800,
+        main: "Snow",
+        description: "clear sky",
+        icon: "01d"
+      }
+    ],
+    clouds: 10,
+    pop: 0,
+    uvi: 6
+  },
+  {
+    dt: 1609740000,
+    sunrise: 1609720900,
+    sunset: 1609759467,
+    temp: {
+      day: 25.57,
+      min: 16.38,
+      max: 27.54,
+      night: 21.26,
+      eve: 23.28,
+      morn: 16.38
+    },
+    feels_like: {
+      day: 23.63,
+      night: 19.3,
+      eve: 21.66,
+      morn: 15.42
+    },
+    pressure: 1014,
+    humidity: 32,
+    dew_point: 7.91,
+    wind_speed: 1.98,
+    wind_deg: 351,
+    weather: [
+      {
+        id: 800,
+        main: "Clouds",
+        description: "clear sky",
+        icon: "01d"
+      }
+    ],
+    clouds: 0,
+    pop: 0,
+    uvi: 6
+  }
+];
